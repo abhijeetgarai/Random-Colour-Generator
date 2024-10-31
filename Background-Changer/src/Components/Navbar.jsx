1098e0841d22e64907b56c800caf130f13b24d75
abhijeetgarai <abhijeetgarai1209@gmail.com>
2024-11-01 01:47:59 +0530

    navbar.jsx

---
+++ Background-Changer/src/Components/Navbar.jsx
@@ -0,0 +1,17 @@
+function Navbar(){
+    return (
+        <>
+        <div className="bg-slate-900">
+
+        <div className="bg-slate-500 rounded-xl p-4 h-3 w-32">
+
+            <div className=""></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        </div>
+        </>
+    )
+}
